393cb8c37cdf033646bd14409159bba68fd285b9
martinaliporata <martinaliporata@gmail.com>
2024-09-12 15:52:35 +0200

    Added google maps API

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,60 +1,35 @@
 @extends('layouts.app')
 @section('content')
 <main>
-   <div class="container home-background">
-       <div class="opaco"></div>
-   </div>
+    <div class="container home-background">
+        <div class="opaco"></div>
+    </div>
 
- <!-- Form di ricerca -->
- <form action="{{ route('search') }}" method="GET">
-    @csrf
-       <div class="container-search">
+    <!-- Form di ricerca -->
+    <form action="{{ route('search') }}" method="GET">
+        @csrf
+        <div class="container-search">
             <div class="container-searchsection">
                 <!-- Barra di ricerca -->
                 <nav class="navbar navbar-custom bg-body-tertiary bg-primary border-radius" data-bs-theme="dark">
                     <div class="container-fluid">
                         <div class="d-flex w-100">
-                            {{-- Changed --}}
                             <input class="form-control me-2 searchbar border-radius" id="autocomplete" name="city" type="search" placeholder="Inserisci un indirizzo o città" aria-label="Search">
                         </div>
                     </div>
                 </nav>
             </div>
-       </div>
+        </div>
 
-       <!-- Sezione Filtri sotto la barra di ricerca -->
-       <div class="container mt-3">
+        <!-- Sezione Filtri sotto la barra di ricerca -->
+        <div class="container mt-3">
             <!-- Dropdown Menu per i servizi -->
-           <!-- Dropdown Menu per i servizi -->
             <div class="dropdown">
                 <button class="btn btn-secondary dropdown-toggle w-100 bg-dark" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
                     Seleziona Servizi
                 </button>
                 <ul class="dropdown-menu w-100" aria-labelledby="dropdownMenuButton">
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="1"> WiFi gratuito</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="2"> Colazione inclusa</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="3"> Aria condizionata</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="4"> Parcheggio gratuito</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="5"> Servizio in camera</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="6"> Animali ammessi</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="7"> Piscina</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="8"> Palestra</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="9"> Spa e centro benessere</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="10"> TV satellitare</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="11"> Minibar</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="12"> Cassaforte in camera</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="13"> Accesso per disabili</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="14"> Deposito bagagli</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="15"> Servizio navetta</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="16"> Noleggio biciclette</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="17"> Area giochi per bambini</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="18"> Sala conferenze</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="19"> Bar</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="20"> Ristorante</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="21"> Servizio lavanderia</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="22"> Asciugacapelli</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="23"> Ferro da stiro</label></li>
-                    <li><label class="dropdown-item"><input type="checkbox" name="services[]" value="24"> Reception 24 ore su 24</label></li>
+                    <!-- ... (i tuoi elementi del menu a discesa) -->
                 </ul>
             </div>
 
@@ -80,10 +55,24 @@
                     </div>
                 </div>
             </div>
-
             <button type="submit" class="btn btn-primary mt-3">Cerca</button>
-       </div>
-   </form>
+        </div>
+    </form>
 </main>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAjVG5FX-7QoyPaSV1uLYIu4B7AiaL69Zw&libraries=places" async defer></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDltqF-8vne8sd6KUee2_G8eWD24UIBYWI&loading=async&libraries=places&callback=initMap" async defer></script>
+<script>
+function initMap() {
+    var input = document.getElementById('autocomplete');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    autocomplete.addListener('place_changed', function() {
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            console.log("Nessun luogo trovato per l'input: '" + place.name + "'");
+            return;
+        }
+        console.log('Luogo selezionato:', place);
+    });
+}
+</script>
 @endsection
