305a0bbb54e2fe69fb835aba2d3da7d8c4076359
JulioGroaz <julio.groaz2020@gmail.com>
2024-09-12 09:48:04 +0200

    search fatta, mancano i servizi

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,54 +4,64 @@
    <div class="container home-background">
        <div class="opaco"></div>
    </div>
-   <div class="container-search">
-        <div class="container-searchsection">
-            <!-- Barra di ricerca -->
-            <nav class="navbar navbar-custom bg-body-tertiary bg-primary border-radius" data-bs-theme="dark">
-                <div class="container-fluid">
-                    <form class="d-flex w-100" role="search">
-                        <input class="form-control me-2 searchbar border-radius" type="search" placeholder="Inserisci un indirizzo o città" aria-label="Search">
-                    </form>
-                </div>
-            </nav>
-        </div>
-   </div>
 
-   <!-- Sezione Filtri sotto la barra di ricerca -->
-   <div class="container mt-3">
-        <!-- Dropdown Menu per i servizi -->
-        <div class="dropdown">
-            <button class="btn btn-dark dropdown-toggle w-100 " type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false" >
-                Seleziona Servizi
-            </button>
-            <ul class="dropdown-menu w-100" aria-labelledby="dropdownMenuButton">
-                <li><a class="dropdown-item " href="#"><input type="checkbox" name="wifi" id="wifi"> WiFi</a></li>
-                <li><a class="dropdown-item" href="#"><input type="checkbox" name="parking" id="parking"> Parcheggio</a></li>
-                <li><a class="dropdown-item" href="#"><input type="checkbox" name="pool" id="pool"> Piscina</a></li>
-                <li><a class="dropdown-item" href="#"><input type="checkbox" name="air_conditioning" id="air_conditioning"> Aria Condizionata</a></li>
-                <li><a class="dropdown-item" href="#"><input type="checkbox" name="pets" id="pets"> Animali ammessi</a></li>
-            </ul>
-        </div>
+ <!-- Form di ricerca -->
+ <form action="{{ route('search') }}" method="GET">
+    @csrf
+       <div class="container-search">
+            <div class="container-searchsection">
+                <!-- Barra di ricerca -->
+                <nav class="navbar navbar-custom bg-body-tertiary bg-primary border-radius" data-bs-theme="dark">
+                    <div class="container-fluid">
+                        <div class="d-flex w-100">
+                            <input class="form-control me-2 searchbar border-radius" name="city" type="search" placeholder="Inserisci un indirizzo o città" aria-label="Search">
+                        </div>
+                    </div>
+                </nav>
+            </div>
+       </div>
 
-        <!-- Campi aggiuntivi per stanze, letti e range di prezzo -->
-        <div class="row mt-3">
-            <div class="col-md-4">
-                <label for="numStanze" class="form-label">Numero di Stanze</label>
-                <input type="number" class="form-control" id="numStanze" placeholder="Inserisci il numero di stanze">
+       <!-- Sezione Filtri sotto la barra di ricerca -->
+       <div class="container mt-3">
+            <!-- Dropdown Menu per i servizi -->
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle w-100 bg-dark" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                    Seleziona Servizi
+                </button>
+                <ul class="dropdown-menu w-100" aria-labelledby="dropdownMenuButton">
+                    <li><a class="dropdown-item" href="#"><input type="checkbox" name="services[]" value="wifi"> WiFi</a></li>
+                    <li><a class="dropdown-item" href="#"><input type="checkbox" name="services[]" value="parking"> Parcheggio</a></li>
+                    <li><a class="dropdown-item" href="#"><input type="checkbox" name="services[]" value="pool"> Piscina</a></li>
+                    <li><a class="dropdown-item" href="#"><input type="checkbox" name="services[]" value="air_conditioning"> Aria Condizionata</a></li>
+                    <li><a class="dropdown-item" href="#"><input type="checkbox" name="services[]" value="pets"> Animali ammessi</a></li>
+                </ul>
             </div>
-            <div class="col-md-4">
-                <label for="numLetti" class="form-label">Numero di Letti</label>
-                <input type="number" class="form-control" id="numLetti" placeholder="Inserisci il numero di letti">
-            </div>
-            <div class="col-md-4">
-                <label for="priceRange" class="form-label">Prezzo (Min-Max)</label>
-                <div class="input-group">
-                    <input type="number" class="form-control" id="priceMin" placeholder="Min €">
-                    <input type="number" class="form-control" id="priceMax" placeholder="Max €">
+
+            <!-- Campi aggiuntivi per stanze, letti, prezzo e numero di persone -->
+            <div class="row mt-3">
+                <div class="col-md-3">
+                    <label for="numStanze" class="form-label">Numero di Stanze</label>
+                    <input type="number" class="form-control" name="numStanze" id="numStanze" placeholder="Inserisci il numero di stanze">
+                </div>
+                <div class="col-md-3">
+                    <label for="numLetti" class="form-label">Posti letto</label>
+                    <input type="number" class="form-control" name="numLetti" id="numLetti" placeholder="Inserisci il numero di letti">
+                </div>
+                <div class="col-md-3">
+                    <label for="numBagni" class="form-label">Bagni</label>
+                    <input type="number" class="form-control" name="numBagni" id="numBagni" placeholder="Inserisci il numero di bagni ">
+                </div>
+                <div class="col-md-3">
+                    <label for="priceRange" class="form-label">Prezzo (Min-Max)</label>
+                    <div class="input-group">
+                        <input type="number" class="form-control" name="priceMin" id="priceMin" placeholder="Min €">
+                        <input type="number" class="form-control" name="priceMax" id="priceMax" placeholder="Max €">
+                    </div>
                 </div>
             </div>
-        </div>
-   </div>
+
+            <button type="submit" class="btn btn-primary mt-3">Cerca</button>
+       </div>
+   </form>
 </main>
 @endsection
-
