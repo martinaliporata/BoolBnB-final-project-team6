77072d7e3fe9680689c46e244a511a8840f2badf
MatteoNeri99 <matteolavoro99@gmail.com>
2024-09-13 09:31:38 +0200

    aggiornata carta in results.blade.php

--- resources/views/admin/apartments/results.blade.php
+++ resources/views/admin/apartments/results.blade.php
@@ -14,16 +14,16 @@
                     <img class="card-img-top" src="{{ $apartment->Img }}" alt="">
                     <div class="card-body">
                         <h5 class="card-title">
-                            {{ $apartment->id }}
+                            {{ $apartment->Nome }}
                         </h5>
                         <p class="card-text">
                             <br> Stanze: {{ $apartment->Stanze }}
                             <br> Letti: {{ $apartment->Letti }}
                             <br> Bagni: {{ $apartment->Bagni }}
-                            <br> Metri quadrati: {{ $apartment->Metri_quadrati }}
+                            <br> Prezzo: &euro;{{ $apartment->Prezzo }}
+                            <br> Metri quadrati: {{ $apartment->Metri_quadrati }} mq
                             <br> Indirizzo: {{ $apartment->Indirizzo }}
-                            <br> Latitudine: {{ $apartment->Latitudine }}
-                            <br> Longitudine: {{ $apartment->Longitudine }}
+
                         </p>
                         <a href="{{ route('apartments.show', $apartment) }}" class="btn btn-primary">Mostra dettagli</a>
                         <a href="{{ route('apartments.edit', $apartment) }}" class="btn btn-warning">Modifica</a>
