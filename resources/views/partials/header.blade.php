3a292e31eaab71dbf7b1050dc7b903a305724a28
ValerioL96 <v.lembo1396@gmail.com>
2024-09-18 08:55:08 +0200

    Modificato navbar in header in modo che " i miei appartamenti" e "cestino" siano visibili solo quando l'utente si registra o accede

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -12,6 +12,12 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav me-auto">
+                <li>
+                    <a class="nav-link" href="{{ url('api/vue-apartments') }}" aria-current="page">
+                        Lista appartamenti
+                    </a>
+                </li>
+
                 @auth
                 <li>
                     <a class="nav-link" href="{{ route('myapp') }}" aria-current="page">
@@ -31,11 +37,6 @@
             <ul class="navbar-nav ms-auto">
                 <!-- Authentication Links -->
                 @guest
-                    @if (Route::has('login'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                    @endif
 
                     @if (Route::has('register'))
                         <li class="nav-item">
